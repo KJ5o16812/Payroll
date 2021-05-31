609a331c50fd78bf81e358ed6e1ae79ab76891f7
KJ5o16812 <woxiaopeng1231@163.com>
2021-05-31 19:39:50 +0800

    验证执行结果

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -15,6 +15,18 @@ public class AddEmployeeTest {
 	//新建添加钟点工操作，并执行
 		Transaction t =new AddHourlyEmployeeTransaction(empId, name ,address, hourlyRate);
 		t.execute();
+		//验证执行结果
+		Employee e=PayrollDatabase.getEmployee(empId);//根据雇员编号读取雇员记录
+		assertNotNull(e);//雇员记录存在
+		assertEquals(name,e.getName());//名字正确
+		assertEquals(address,e.getAddres());
+		PaymentClassification pc =e.getPaymentClassification();
+		assertTrue(pc instanceof HourlyClassification);//钟点工
+		HourlyClassification hc =(HourlyClassification) pc;
+		assertEquals(hourlyRate,hc.getHourlyRate(),0.01);//小时工资正确
+		PaymentMethod pm = e.getPaymentMethod();
+		assertTrue(pm instanceof HoldMethod);//支付方式默认为保存支票
+		
 	}
 	@Test
 	public void test() {
