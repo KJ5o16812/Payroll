e399650c56864e2857b4b379fb458197fbd48ce5
KJ5o16812 <woxiaopeng1231@163.com>
2021-05-31 17:35:59 +0800

    小时工的基本信息

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -6,6 +6,13 @@ import org.junit.Test;
 
 public class AddEmployeeTest {
 
+	//添加小时工资雇员
+	public void testAddHourlyEmployeeTransaction(){
+		int empId = 1001;
+		String name = "Bill";
+		String address = "Home";
+		double hourlyRate = 12.5;		
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
