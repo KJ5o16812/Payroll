f06c8354c2047a47340f42de7bca0e6437caeecb
KJ5o16812 <woxiaopeng1231@163.com>
2021-05-31 19:29:07 +0800

    添加钟点工操作，并执行

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -11,7 +11,10 @@ public class AddEmployeeTest {
 		int empId = 1001;
 		String name = "Bill";
 		String address = "Home";
-		double hourlyRate = 12.5;		
+		double hourlyRate = 12.5;	
+	//新建添加钟点工操作，并执行
+		Transaction t =new AddHourlyEmployeeTransaction(empId, name ,address, hourlyRate);
+		t.execute();
 	}
 	@Test
 	public void test() {
