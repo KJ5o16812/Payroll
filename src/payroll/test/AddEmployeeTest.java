ba2aad59f49be40c5c5485016cbdea9a98f5a00a
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-01 18:01:58 +0800

    添加月薪雇员

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -37,6 +37,28 @@ public class AddEmployeeTest {
 		assertTrue(pm instanceof HoldMethod);//支付方式默认为保存支票
 		
 	}
+	//添加月薪雇员
+	public void testAddSalariedEmployee(){
+		int empId = 1002;
+		String name = "Bill";
+		String address = "Home";
+		double salary = 2410.0;	
+	//新建添加钟点工操作，并执行
+		Transaction t =new AddSalariedEmployeeTransaction(empId, name ,address, salary);
+		t.execute();
+		//验证执行结果
+		Employee e=PayrollDatabase.getEmployee(empId);//根据雇员编号读取雇员记录
+		assertNotNull(e);//雇员记录存在
+		assertEquals(name,e.getName());//名字正确
+		assertEquals(address,e.getAddress());
+		PaymentClassification pc =e.getPaymentClassification();
+		assertTrue(pc instanceof SalariedClassification());//钟点工
+		SalariedClassification hc =(SalariedClassification) pc;
+		assertEquals(salary,hc.getSalary(),0.01);//小时工资正确
+		PaymentMethod pm = e.getPaymentMethod();
+		assertTrue(pm instanceof HoldMethod);//支付方式默认为保存支票
+		
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
