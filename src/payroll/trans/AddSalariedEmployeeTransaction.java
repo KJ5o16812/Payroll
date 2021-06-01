edafbbc5b685a3936f07220a1ce33751e5157840
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-01 18:20:26 +0800

    赋值给字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -4,8 +4,17 @@ import payroll.Transaction;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double salary;
+
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
+		this.empId = empId;
 		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.address = address;
+		this.salary = salary;
 	}
 
 	@Override
