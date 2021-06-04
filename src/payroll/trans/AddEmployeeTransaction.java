eb4905ce12de9ac01243353afb0a3969205d5d0c
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-04 22:30:04 +0800

    用字段生成函数

--- src/payroll/trans/AddEmployeeTransaction.java
+++ src/payroll/trans/AddEmployeeTransaction.java
@@ -11,6 +11,13 @@ public abstract class AddEmployeeTransaction implements Transaction   {
 
 	protected int empId;
 
+	public AddEmployeeTransaction(int empId, String name, String address) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+	}
+
 	protected abstract PaymentClassification getPaymentClassification();
 
 	protected String name;
