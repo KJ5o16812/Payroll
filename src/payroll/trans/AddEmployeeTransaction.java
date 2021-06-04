5b6dd43d12b2a869e90ecf1bcc815b82677b27f5
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-04 22:21:22 +0800

    用超类代替

---
+++ src/payroll/trans/AddEmployeeTransaction.java
@@ -0,0 +1,33 @@
+package payroll.trans;
+
+import payroll.Employee;
+import payroll.PaymentClassification;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.classification.SalariedClassification;
+import payroll.method.HoldMethod;
+
+public abstract class AddEmployeeTransaction {
+
+	protected int empId;
+
+	protected abstract PaymentClassification getPaymentClassification();
+
+	protected String name;
+	protected String address;
+
+	public AddEmployeeTransaction() {
+		super();
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		Employee employee =new Employee(empId,name,address);
+		employee.setPaymentClassification(getPaymentClassification());
+		employee.setPaymentMethod(new HoldMethod());
+		PayrollDatabase.save(employee);
+	
+	}
+
+}
--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,18 +1,12 @@
 package payroll.trans;
 
-import payroll.Employee;
 import payroll.PaymentClassification;
-import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.HourlyClassification;
 import payroll.classification.SalariedClassification;
-import payroll.method.HoldMethod;
 
-public class AddSalariedEmployeeTransaction implements Transaction {
+public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
@@ -24,14 +18,6 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		Employee employee =new Employee(empId,name,address);
-		employee.setPaymentClassification(getPaymentClassification());
-		employee.setPaymentMethod(new HoldMethod());
-		PayrollDatabase.save(employee);
-
-	}
 	protected PaymentClassification getPaymentClassification(){
 		return new SalariedClassification(salary);
 	}
