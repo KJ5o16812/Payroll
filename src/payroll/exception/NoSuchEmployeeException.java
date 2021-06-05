b36be3184d4cde32018d3a661aa22bb1e50a73a6
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-05 16:12:27 +0800

    添加构造方法

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -2,4 +2,17 @@ package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
 
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public NoSuchEmployeeException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
 }
