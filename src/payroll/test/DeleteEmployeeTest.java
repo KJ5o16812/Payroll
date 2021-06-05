9326f76a7471e264f2038ee19dbeb514c12611fe
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-05 15:54:02 +0800

    先插入一个雇员

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -4,9 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import payroll.trans.AddHourlyEmployeeTransaction;
+
 public class DeleteEmployeeTest {
 
 	@Test
+	public void testDeleteEmployeeExisted(){
+		int empId=2001;
+		new AddHourlyEmployeeTransaction(empId,"Bill","Home",12.5).execute();
+		
+	}
 	public void test() {
 		fail("Not yet implemented");
 	}
