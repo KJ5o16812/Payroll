4e2aff26e05711a08775acfb342d872893a23a57
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-06 22:12:12 +0800

    月薪雇员(邮寄支票)

--- src/payroll/test/PaydayTest.java
+++ src/payroll/test/PaydayTest.java
@@ -29,7 +29,22 @@ public class PaydayTest {
 		
 	}
 	
-	public void testPaydaySalariedMailMethod(){}
+	public void testPaydaySalariedMailMethod(){
+		int empId=60011;
+		String name="Bill";
+		double salary=3456.7;
+		new AddSalariedEmployeeTransaction(empId,name,"Home",salary).execute();
+		new ChangeMailTransaction(empId,"BZU").execute();
+		PaydayTransaction t = new PaydayTransaction();
+		t.execute();
+		Paycheck p =t.getPaycheck(empId);
+		assertEquals(empId,p.getEmpId());
+		assertEquals(name,p.getName());
+		assertEquals(salary,p.getAmount(),0.01);
+		assertNotNull(p.getDisposition());
+		Employee employee=PayrollDatabase.getEmployee(empId);
+		assertEquals(employee.getPaymentMethod().getDisposition(),p.getDisposition());
+	}
 	
 	public void testPaydaySalariedDirectMethod(){}
 	
