98a78683ce65fcf55b351e2e4b06ec9c1c56897b
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-06 11:07:54 +0800

    月薪雇员（保留支票）

--- src/payroll/test/PaydayTest.java
+++ src/payroll/test/PaydayTest.java
@@ -4,9 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
+import payroll.trans.AddSalariedEmployeeTransaction;
+
 public class PaydayTest {
 
-	public void testPaydaySalaried(){}
+	public void testPaydaySalaried(){
+		int empId=6001;
+		String name="Bill";
+		double salary=3456.7;
+		new AddSalariedEmployeeTransaction(empId,name,"Home",salary).execute();
+		PaydayTransaction t = new PaydayTransaction();
+		t.execute();
+		Paycheck p =t.getPaycheck(empId);
+		assertEquals(empId,p.getEmpId());
+		assertEquals(name,p.getName());
+		assertEquals(salary,p.getAmount(),0.01);
+		assertNotNull(p.getDisposition());
+		Employee employee=PayrollDatabase.getEmployee(empId);
+		assertEquals(employee.getPaymentMethod().getDisposition(),p.getDisposition());
+		
+	}
 	
 	public void testPaydaySalariedMailMethod(){}
 	
