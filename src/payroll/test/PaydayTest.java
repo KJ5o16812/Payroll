eded8dc335026ab503004f5607a6f8be8e26f0d0
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-06 10:42:51 +0800

    计算工资

---
+++ src/payroll/test/PaydayTest.java
@@ -0,0 +1,31 @@
+package payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaydayTest {
+
+	public void testPaydaySalaried(){}
+	
+	public void testPaydaySalariedMailMethod(){}
+	
+	public void testPaydaySalariedDirectMethod(){}
+	
+	public void testPaydayHourlyNoTimeCard(){}
+	
+	public void testPaydayHourlylessThan8Hours(){}
+	
+	public void testPaydayHourlyGreatThan8Hours(){}
+	
+	public void testPaydayHourlyMoreTimeCards(){}
+	
+	public void testPaydayCommissionedNoSalesReceipts(){}
+	
+	public void testPaydayCommissionedMoreSalesReceipts(){}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
