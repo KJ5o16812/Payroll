8c67fdec2c89b7b19a8466df5d70c381c9731499
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-06 22:04:55 +0800

    实现calculatePay方法

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -14,5 +14,8 @@ public class SalariedClassification extends PaymentClassification {
 		// TODO Auto-generated method stub
 		return salary;
 	}
+	public double calculatePay(){
+		return 0;
+	}
 
 }
