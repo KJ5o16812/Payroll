34578b171550f674a945d5a796242146e41da2fb
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-07 15:21:52 +0800

    创建类

---
+++ src/payroll/method/DirectMethod.java
@@ -0,0 +1,30 @@
+package payroll.method;
+
+import payroll.PaymentMethod;
+
+public class DirectMethod extends PaymentMethod {
+	private String bank;
+	private String account;
+	
+	public DirectMethod(String bank,String account){
+		this.bank=bank;
+		this.account=account;
+		
+		
+	}
+	public String getBank(){
+		return bank;
+	}
+	public String getAccount(){
+		return account;
+		
+	}
+	@Override
+	public String getDisposition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
