ac4308ac137c2351882914be1bb81536a853de89
KJ5o16812 <woxiaopeng1231@163.com>
2021-06-07 15:24:58 +0800

    创建类

---
+++ src/payroll/method/MailMethod.java
@@ -0,0 +1,22 @@
+package payroll.method;
+
+import payroll.PaymentMethod;
+
+public class MailMethod extends PaymentMethod {
+
+	private String address;
+	
+	public MailMethod(String address){
+		this.address = address;
+		
+		
+	}
+	public String getAddress(){
+		return address;
+	}
+	public String getDisposition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
